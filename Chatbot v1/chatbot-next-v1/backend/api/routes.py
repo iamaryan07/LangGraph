d81dd378dadebb3e56929f536a16b71574b43734
iamaryan07 <aryanbalhara3700@gmail.com>
2026-05-27 00:31:03 +0530

    Removed print statements from STM

--- Chatbot v1/chatbot-next-v1/backend/api/routes.py
+++ Chatbot v1/chatbot-next-v1/backend/api/routes.py
@@ -59,10 +59,6 @@ async def chat(req: ChatRequest, request: Request):
             "type": "interrupt",
             "interrupt": interrupt_data
         }
-    
-    state = await chatbot.aget_state(config)
-
-    print(state.values)
 
     last_message = response["messages"][-1]
 
